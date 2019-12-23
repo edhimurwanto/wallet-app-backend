90e2813debaa2119ec1f64cdab4e5dd277805ceb
edhiuchiha <uchihaedhi27@gmail.com>
2019-12-23 09:29:38 +0700

    [DB-CONFIG] change db connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,16 +1,16 @@
 import { createConnection as connect } from 'typeorm';
 import entities from './entities';
 
-async function createConnection(){
+async function createConnection() {
     const connection = await connect({
-        type: process.env.DB_DRIVER,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database:process.env.DB_NAME,
-        synchronize:process.env.DB_SYNC,
-        logging:process.env.DB_LOGGING,
+        type: 'mysql',
+        host: 'localhost',
+        port: '3306',
+        username: 'root',
+        password: '',
+        database: 'db_cinema',
+        synchronize: true,
+        logging: false,
         entities: Object.values(entities),
     });
 
